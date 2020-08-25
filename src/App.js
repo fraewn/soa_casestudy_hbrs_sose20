70ee0beeb0a2a0dae4d0235942e91d897445f128
Franziska Kuesters <franziska.kuesters@smail.inf.h-brs.de>
2020-08-25 12:15:30 +0200

    rest api sample code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+      isLoaded: false,
+    };
+  }
+
+  // TO USE THIS YOU NEED TO CRATE SPECIAL REACT-API APP NOT NORMAL REACT APP SO THIS WON'T WORK
+  componentDidMount() {
+    // get data from restendpoint
+    fetch("https://jsonplaceholder.typicode.com/users")
+      // save data in variable res and convert res to json format
+      .then((res) => res.json())
+      // save formatted data in current state
+      .then((json) => {
+        this.setState({
+          // update isLoaded variable
+          isLoaded: true,
+          // save data from outside to application
+          item: json,
+        });
+      });
+  }
+
+  render() {
+    // important to check if loaded
+    // because mount method comes AFTER render method (so first time data won't be there yet)
+
+    // map function creates new array with inputted data
+
+    var { isLoaded, item } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <u1>
+            {item.map((item) => (
+              <li key={item.id}>
+                Name: {item.name} | Email: {item.email}
+              </li>
+            ))}
+            ;
+          </u1>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/components/camundaApi.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class CamundaApi extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default CamundaApi;
